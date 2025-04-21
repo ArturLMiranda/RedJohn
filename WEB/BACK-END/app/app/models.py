1bdbd8c46310ad09b84ac474dc923b73f3cfa176
Artur Lacerda <artur.lacerda@outlook.com>
2025-04-21 08:51:26 -0300

    db

---
+++ WEB/BACK-END/app/app/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+class Demandante(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Tipo(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class LoginUsuario(models.Model):
+    nome = models.CharField(max_length=255)
+    senha = models.CharField(max_length=64)  # SHA-256
+
+    def __str__(self):
+        return self.nome
+
+
+class Responsavel(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Atividade(models.Model):
+    descricao = models.TextField()
+    validade = models.DateField()
+    demandante = models.ForeignKey(Demandante, on_delete=models.CASCADE)
+    responsaveis = models.ManyToManyField(Responsavel, through='AtividadeResponsavel')
+
+    def __str__(self):
+        return f'{self.descricao[:30]}...'
+
+
+class AtividadeResponsavel(models.Model):
+    atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE)
+    responsavel = models.ForeignKey(Responsavel, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('atividade', 'responsavel')
+
+
+class TipoLogin(models.Model):
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    login = models.ForeignKey(LoginUsuario, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('tipo', 'login')
